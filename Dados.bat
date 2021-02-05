3cebd4a1fadf4a6c2733080ac881910a688fad33
gabucho1985 <56567498+gabucho1985@users.noreply.github.com>
2021-02-05 00:59:45 -0500

    Files used  in the code
    These are the files that I used in the code where I compare folder names after converting them into a list then get the "MATCH" column from EDD CASES DECEMBER 18TH 2020.xlsx and added them at the end of the path and wrote them into a batch file in order to move folders more efficiently.

---
+++ Dados.bat
@@ -0,0 +1,16 @@
+powershell -Command "Copy-Item \\isb-51JFKFS\BSA\BSA_EDD\Gabriel33\221500642 C:\Users\GMacias\newfolder"
+powershell -Command Robocopy '\\isb-51JFKFS\BSA\BSA_EDD\Gabriel33\221500642' 'C:\Users\GMacias\newfolder\221500642' /S /E /MT:32 /NFL /NDL /NJH /NJS
+powershell -Command "Copy-Item \\isb-51JFKFS\BSA\BSA_EDD\Gabriel33\204968223 C:\Users\GMacias\newfolder"
+powershell -Command Robocopy '\\isb-51JFKFS\BSA\BSA_EDD\Gabriel33\204968223' 'C:\Users\GMacias\newfolder\204968223' /S /E /MT:32 /NFL /NDL /NJH /NJS
+powershell -Command "Copy-Item \\isb-51JFKFS\BSA\BSA_EDD\Gabriel33\202208566 C:\Users\GMacias\newfolder"
+powershell -Command Robocopy '\\isb-51JFKFS\BSA\BSA_EDD\Gabriel33\202208566' 'C:\Users\GMacias\newfolder\202208566' /S /E /MT:32 /NFL /NDL /NJH /NJS
+powershell -Command "Copy-Item \\isb-51JFKFS\BSA\BSA_EDD\Gabriel33\111672795 C:\Users\GMacias\newfolder"
+powershell -Command Robocopy '\\isb-51JFKFS\BSA\BSA_EDD\Gabriel33\111672795' 'C:\Users\GMacias\newfolder\111672795' /S /E /MT:32 /NFL /NDL /NJH /NJS
+powershell -Command "Copy-Item \\isb-51JFKFS\BSA\BSA_EDD\Gabriel33\461435360 C:\Users\GMacias\newfolder"
+powershell -Command Robocopy '\\isb-51JFKFS\BSA\BSA_EDD\Gabriel33\461435360' 'C:\Users\GMacias\newfolder\461435360' /S /E /MT:32 /NFL /NDL /NJH /NJS
+powershell -Command "Copy-Item \\isb-51JFKFS\BSA\BSA_EDD\Gabriel33\222216876 C:\Users\GMacias\newfolder"
+powershell -Command Robocopy '\\isb-51JFKFS\BSA\BSA_EDD\Gabriel33\222216876' 'C:\Users\GMacias\newfolder\222216876' /S /E /MT:32 /NFL /NDL /NJH /NJS
+powershell -Command "Copy-Item \\isb-51JFKFS\BSA\BSA_EDD\Gabriel33\371593029 C:\Users\GMacias\newfolder"
+powershell -Command Robocopy '\\isb-51JFKFS\BSA\BSA_EDD\Gabriel33\371593029' 'C:\Users\GMacias\newfolder\371593029' /S /E /MT:32 /NFL /NDL /NJH /NJS
+powershell -Command "Copy-Item \\isb-51JFKFS\BSA\BSA_EDD\Gabriel33\222012591 C:\Users\GMacias\newfolder"
+powershell -Command Robocopy '\\isb-51JFKFS\BSA\BSA_EDD\Gabriel33\222012591' 'C:\Users\GMacias\newfolder\222012591' /S /E /MT:32 /NFL /NDL /NJH /NJS
